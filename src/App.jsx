c06992cb997d97431dab28867133404bb42bd5b7
Shivam <shivambhalla1499@gmail.com>
2021-05-25 15:25:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
-import './index.css';
+import React from "react";
 
 const App = () => {
 
-const [increment,setIncrement] = useState(0);
+    const socks = [1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4];
 
-const ClickCount =()=>
-{
-    setIncrement(increment+1);  
+    console.log(socks);
+
+    socks.sort();
+
+    console.log(socks);
+
+    socks.sort((a, b) => {
+        return a - b;
+    });
+
+    console.log(socks);
+
+    let pairCount = 0;
+
+    for (var i = 0; i < socks.length; i++) {
+        if (socks[i] === socks[i + 1]) {
+            pairCount++;
+            i++;
+        }
+    }
+    console.log(pairCount)
+
+    return (<div>
+        <h1>Array of Socks is: </h1>
+        <div style={{ display: "flex" }}>
+
+            {
+                socks.map((arr) => {
+                    return <h2>{arr}{","}</h2>
+                })
+            }
+
+        </div>
+
+        <h1>Number of pairs for every unique color are:</h1>
+
+        <h2>{pairCount}</h2>
+
+    </div>);
 };
-    return (
-        <>
-        <h1>
-        &nbsp;&nbsp;&nbsp;&nbsp;{increment}&nbsp;&nbsp;&nbsp;&nbsp;
-        </h1>
-        <button onClick={ClickCount}>Click ME</button>
-        </>
-    )
-}
 
 export default App;
